7790334a9cbae7e4e06247152aa08f41784b6254
summer19417 <summerpolk808@gmail.com>
2017-03-10 14:28:28 -1000

    I finnished all the things

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
+console.log("Hello World");
 function fortuneTeller(){
-
+	var numOfKids = 0;
+	var partnerName = "Kaneki";
+	var geographiclocation = "Paris";
+	var jobTitle = " manga creater";
+	alert("You will be a" + jobTitle + " X in Y, and married to Z with N kids.");
 }
+
+fortuneTeller(); 
 
 function ageCalculator(){
-
+	var BirthYear = 2002;
+	var FutureYear = 2020;
+	alert("You were born " + BirthYear + " and in " + FutureYear + " you will be 16 or 17.")
+		;
 }
+
+ageCalculator();  
+
